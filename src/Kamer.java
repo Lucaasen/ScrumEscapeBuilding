8b8b032ab317af99da9510d9dafb4c42bfbcb8da
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-05-19 12:45:24 +0200

    Updated Monster

--- src/Kamer.java
+++ src/Kamer.java
@@ -1,20 +1,38 @@
 public class Kamer {
-    public static void Kamer(String[] args){
-        String doel;
-        String informatie;
-        String opdracht;
-        int kamerNummer;
-        Monster monster = new Monster();
-        Deur deur = new Deur();
+    private String doel;
+    private String informatie;
+    private String opdracht;
+    private int kamerNummer;
+    private Monster monster;
+    private Deur deur;
+    private KamerType kamerType;
 
-        new Opdracht();
-
-
+    public Kamer(String doel, String informatie, String opdracht, int kamerNummer, KamerType kamerType) {
+        this.doel = doel;
+        this.informatie = informatie;
+        this.opdracht = opdracht;
+        this.kamerNummer = kamerNummer;
+        this.monster = null;
+        this.deur = new Deur();
+        this.kamerType = kamerType;
     }
-    public String getKamerType(){
-        String kamerType;
-        KamerType kamerType = new KamerType();
-        new KamerType();
-        Return(Type);
+
+    public KamerType getKamerType() {
+        return this.kamerType;
+    }
+
+    public void spawnMonster(String naam, String reden) {
+        this.monster = new Monster(naam, reden);
+        monster.toonMonster();
+    }
+
+    public boolean isGeblokkeerd() {
+        return monster != null && monster.isActief();
+    }
+
+    public void verslaMonster() {
+        if (monster != null) {
+            monster.versla();
+        }
     }
 }
--- src/Monster.java
+++ src/Monster.java
@@ -1,9 +1,34 @@
 public class Monster {
-    int levens;
-    String type;
-    String spawn;
+    private int levens;
+    private String naam;
+    private String reden;
+    private boolean actief;
+
+    public Monster(String naam, String reden) {
+        this.naam = naam;
+        this.reden = reden;
+        this.levens = 1;
+        this.actief = true;
+    }
+
+    public void toonMonster() {
+        System.out.println("⚠️ Monster verschenen: " + naam);
+        System.out.println("Belemmering: " + reden);
+    }
+
+    public boolean isActief() {
+        return actief;
+    }
+
+    public void versla() {
+        this.actief = false;
+        System.out.println("✅ Je hebt het monster '" + naam + "' verslagen!");
+    }
+
+    public void schade(int hoeveelheid) {
+        levens -= hoeveelheid;
+        if (levens <= 0) {
+            versla();
+        }
+    }
 }
-
-
-
- 
